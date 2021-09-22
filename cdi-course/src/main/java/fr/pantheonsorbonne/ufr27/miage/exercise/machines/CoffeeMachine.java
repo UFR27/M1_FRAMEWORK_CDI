6ca5f9e5788a348f37ac1c17afccaeb20138c5c0
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2021-09-23 00:17:38 +0200

    coffe

---
+++ cdi-course/src/main/java/fr/pantheonsorbonne/ufr27/miage/exercise/machines/CoffeeMachine.java
@@ -0,0 +1,9 @@
+package fr.pantheonsorbonne.ufr27.miage.exercise.machines;
+
+import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.Coffee;
+
+public interface CoffeeMachine {
+
+	Coffee getCoffee();
+
+}
